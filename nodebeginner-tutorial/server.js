1f59019efbc3c26de8432966f78865fcdaf8a032
Brandon Yee <yee.brandon.j@gmail.com>
2018-04-23 00:43:13 -0500

    initial commit

---
+++ nodebeginner-tutorial/server.js
@@ -0,0 +1,23 @@
+// Tutorial: https://www.nodebeginner.org/blog/post/nodejs-tutorial-how-to-work-with-websockets/
+
+var app = require('express')();
+var http = require('http').Server(app);
+// Serves client at: http://localhost:8000/socket.io/socket.io.js
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('A new WebSocket connection has been established');
+});
+
+setInterval(function() {
+    var stockprice = Math.floor(Math.random() * 1000);
+    io.emit('stock price update', stockprice);
+}, 50);
+
+http.listen(8000, function() {
+  console.log('Listening on *:8000');
+});
